2251d331cd7f96adceed50f3d8db9a162011860e
alemusu <alemusu2002@gmail.com>
2024-12-27 23:49:42 +0100

    - Finished all parameters getters&setters  - Added an age param  - Started adding functionality

--- todo.py
+++ todo.py
@@ -16,8 +16,9 @@ class Priority(Enum):
 
 
 class Item():
-    __creation_date = date.today()
     __title = "empty"
+    __id = ""
+    __creation_date = date.today()
     __status = Status.NOT_STARTED
     __priority = Priority.LOW
     __flag = False
@@ -25,7 +26,7 @@ class Item():
     __due_date = date
     __state = False
     __notes = ""
-    __incon = ""
+    __icon = ""
     
 
     def __init__(self, title:str=None):
@@ -39,6 +40,10 @@ class Item():
         """Returns the title of the item"""
         return self.__title
     @property
+    def id(self)->str:
+        """Returns the id of the item"""
+        return self.__id
+    @property
     def creation_date(self)->date:
         """Returns the creation date of the item"""
         return self.__creation_date
@@ -51,9 +56,33 @@ class Item():
         """Returns the priority of the item"""
         return self.__priority
     @property
+    def flag(self)->bool:
+        """Returns the flag of the item"""
+        return self.__flag
+    @property
+    def url(self)->str:
+        """Returns the url of the item"""
+        return self.__url
+    @property
+    def due_date(self)->date:
+        """Returns the due date of the item"""
+        return self.__due_date
+    @property
+    def state(self)->bool:
+        """Returns the state of the item"""
+        return self.__state
+    @property
     def notes(self)->str:
         """Returns the notes of the item"""
         return self.__notes
+    @property
+    def icon(self)->str:
+        """Returns the icon of the item"""
+        return self.__icon
+    @property
+    def age(self)->date:
+        """Returns the age of the item"""
+        return self.creation_date() - date.today()
 
 
     @title.setter
@@ -68,8 +97,41 @@ class Item():
     @priority.setter
     def priority(self, value):
         self.__priority = value
+    @flag.setter
+    def flag(self, value):
+        self.__flag = value
+    @url.setter
+    def url(self, value):
+        self.__url = value
+    @due_date.setter
+    def due_date(self, value):
+        self.__due_date= value
+    @state.setter
+    def state(self, value):
+        self.__state = value
     @notes.setter
     def notes(self, value):
         self.__notes = value
+    @icon.setter
+    def icon(self, value):
+        self.__icon = value
 
     
+class Todo():
+    __todos = []
+
+
+    def __init__(self):
+        print("new todo list created")
+        self._current = -1
+
+
+    def new_item(self, item:Item):
+        self.__todos.append(item)
+
+
+    @property
+    def items(self)->list:
+        print("*"*80)
+        for item in self.__todos:
+            print(item.title(), item.status(), item.priority(), item.age())
